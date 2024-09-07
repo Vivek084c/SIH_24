20000634b8472170afdf671f01482488826eda71
Vivek Choudhry <126272453+Vivek084c@users.noreply.github.com>
2024-09-07 23:01:06 +0530

    unknown update

--- temp.py
+++
@@ -1,8 +0,0 @@
-import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer
-
-model_name = "microsoft/phi-2"
-
-model = AutoModelForCausalLM.from_pretrained(model_name)
-model.save_pretrained(f"cache/model/{model_name}")
-
